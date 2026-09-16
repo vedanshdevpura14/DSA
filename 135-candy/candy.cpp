a922179e2151b1bfba8a7a6ff95ffdd2b5f4b1c3
Vedansh Devpura <devpuravedansh@gmail.com>
2026-09-16 18:47:44 +0530

    Time: 8 ms (16.38%) | Memory: 68.5 MB (42.40%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        int left[n];
+        int right[n];
+        left[0]=1 , right[n-1]=1;
+        for(int i = 1 ; i < n ; i++){
+            if(ratings[i]>ratings[i-1]){
+                left[i]= left[i-1]+1;
+            }
+            else{
+                left[i]=1;
+            }
+        }
+        for(int i = n-2 ; i >=0 ; i--){
+            if(ratings[i]>ratings[i+1]){
+                right[i]= right[i+1]+1;
+            }
+            else{
+                right[i]=1;
+            }
+        }
+        int sum = 0;
+        for(int i = 0 ; i < n; i++){
+            sum+= max(left[i], right[i]);
+        }
+        return sum;
+    }
+};
